f874835315df97b22e41c333c67e0c573384bcc6
Reetam101 <reetamraj2@gmail.com>
2022-04-21 12:45:00 +0530

    pushing to heroku

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running on ${PORT}`))
 
 const io = require('socket.io')(server, {
